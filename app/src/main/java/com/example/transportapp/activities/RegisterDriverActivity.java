555b9b106b77c61b50b5238681e5839a45890165
Julio <jcrr.11@hotmail.com>
2020-10-02 00:02:14 -0500

    Creado: Vista de registro conductor, botones de cerrar y devolver agregados.

---
+++ app/src/main/java/com/example/transportapp/activities/RegisterDriverActivity.java
@@ -0,0 +1,15 @@
+package com.example.transportapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.transportapp.R;
+
+public class RegisterDriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_driver);
+    }
+}
